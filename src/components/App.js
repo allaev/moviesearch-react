c298d868e51da55b4911ba3fc46c61e1aa8a287d
allaev <34710484+allaev@users.noreply.github.com>
2019-04-17 23:20:52 +0500

    create event handlers

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
+import tmdb from '../api/tmdb';
 
 class App extends Component {
+  state = { movies: [] };
+
+  onTermSubmit = async term => {
+    const response = await tmdb.get('/movie', {
+      params: {
+        query: term
+      }
+    });
+
+    this.setState({
+      movies: response.data.results
+    });
+  };
+
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.props.onFormSubmit(this.state.term);
+  };
+
+  onInputChange = event => {
+    this.setState({term: event.target.value});
+  };
+
   render() {
     return (
       <div>
-        SearchBar
+        <form onSubmit={this.onFormSubmit}>
+          <input value={this.state.term} onChange={this.onInputChange} type="text" placeholder="Enter the movie name" aria-label="Search term" />
+          <button type="submit">Search</button>
+        </form>
       </div>
     )
   }
